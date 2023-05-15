9eb34f1f70ff09be17a2911d47cbbdf2813da26c
danielname <daniel.v.name@gmail.com>
2023-05-15 18:57:51 -0500

    Lesson - Variable Syntax

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,10 @@ fun myName(arg: String){
 }
 
 fun main(args: Array<String>) {
+    val x=2 //type inferred because variable (constant) defined and set
+    val y:Int //type declared (I in Int MUST BE CAPITALIZED)
+    val z:Int=2 //this is fine, but unnecessary
+//    val w:int=4.6 //type mismatch, will not compile, NO IMPLICIT CASTING
     println("Hello\nWorld!")
     myName("Daniel")
 }
