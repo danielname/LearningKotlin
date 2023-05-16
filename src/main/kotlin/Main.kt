300297e0400b01b813c14d34f80b66d118df66c6
danielname <daniel.v.name@gmail.com>
2023-05-15 19:03:14 -0500

    New Code - Exercise - Completed Circle Exercise

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,17 @@ fun myName(arg: String){
     println("**********\n* $arg *\n**********")
 }
 
+fun circle(){
+    val raduis = (Math.random() * 10) + 1
+    println(raduis)
+    println(raduis * raduis * Math.PI)
+}
+
 fun main(args: Array<String>) {
     val x=2 //type inferred because variable (constant) defined and set
     val y:Int //type declared (I in Int MUST BE CAPITALIZED)
     val z:Int=2 //this is fine, but unnecessary
 //    val w:int=4.6 //type mismatch, will not compile, NO IMPLICIT CASTING
     println("Hello\nWorld!")
-    myName("Daniel")
+    circle()
 }
